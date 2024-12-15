ce20dbfcf3ecff047a45ffcd73048ea501d916d0
Nicolas Pires <ntconsult.pires@agi.com.br>
2024-12-15 17:26:51 -0300

    add modelMapper config

---
+++ src/main/java/br/com/nksolucoes/nkorderms/config/AppConfig.java
@@ -0,0 +1,20 @@
+package br.com.nksolucoes.nkorderms.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AppConfig {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		final ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+	}
+
+}
