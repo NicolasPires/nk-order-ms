45afee3685bc92ee28b7de47b933adb0de643790
Nicolas Pires <ntconsult.pires@agi.com.br>
2024-12-15 15:25:00 -0300

    initialize project

---
+++ src/main/java/br/com/nksolucoes/nk_order_ms/NkOrderMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.nksolucoes.nk_order_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkOrderMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NkOrderMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nksolucoes/nk_order_ms/NkOrderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nksolucoes.nk_order_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkOrderMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
