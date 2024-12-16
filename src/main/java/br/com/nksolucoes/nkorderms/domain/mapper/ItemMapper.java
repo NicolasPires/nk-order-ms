7e83682c4dc6eeef6f19bb3d5dbf3c2c15942f9f
Nicolas Pires <ntconsult.pires@agi.com.br>
2024-12-16 16:08:53 -0300

    fix removing unnecessaring annotation

--- src/main/java/br/com/nksolucoes/nkorderms/domain/mapper/ItemMapper.java
+++ src/main/java/br/com/nksolucoes/nkorderms/domain/mapper/ItemMapper.java
@@ -4,14 +4,9 @@ import br.com.nksolucoes.nkorderms.domain.model.Item;
 import br.com.nksolucoes.nkorderms.domain.records.request.ItemRequest;
 import br.com.nksolucoes.nkorderms.domain.records.response.ItemResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
-
-	@Mapping(target = "itemId", ignore = true)
-	@Mapping(target = "subtotal", ignore = true)
 	Item requestToEntity(ItemRequest request);
-
 	ItemResponse entityToResponse(Item item);
 }
--- src/main/java/br/com/nksolucoes/nkorderms/domain/mapper/OrderMapper.java
+++ src/main/java/br/com/nksolucoes/nkorderms/domain/mapper/OrderMapper.java
@@ -4,14 +4,9 @@ import br.com.nksolucoes.nkorderms.domain.model.Order;
 import br.com.nksolucoes.nkorderms.domain.records.request.OrderRequest;
 import br.com.nksolucoes.nkorderms.domain.records.response.OrderResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {ItemMapper.class})
 public interface OrderMapper {
-
-	@Mapping(target = "totalAmount", ignore = true)
-	@Mapping(target = "orderStatus", ignore = true)
 	Order requestToEntity(OrderRequest request);
-
 	OrderResponse entityToResponse(Order order);
 }
