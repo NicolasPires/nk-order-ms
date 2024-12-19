fd4880012ebbc5410d66fc392e9cb402ba01b44f
Nicolas Pires <ntconsult.pires@agi.com.br>
2024-12-19 13:34:19 -0300

    add conextLoads test

--- src/main/java/br/com/nksolucoes/nkorderms/NkOrderMsApplication.java
+++ src/main/java/br/com/nksolucoes/nkorderms/NkOrderMsApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.nksolucoes.nkorderms")
 @OpenAPIDefinition(info = @Info(title = "nk-order-ms", version ="1", description = "API developed for knowledge presentation"))
 public class NkOrderMsApplication {
 
